db7c354a97f693d155cb22a8c68530ac9eb11f29
sia <stolajruga@yandex.ru>
2021-01-22 14:30:17 +0300

    This added debugger

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'SiteController@index');
+Route::get('/catalog/{category}', function ($category){
+    dump($category);
 });
