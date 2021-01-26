0d038b2d22ef9abefdc0e6be87166c17aa039467
siass <stolajruga@yande.ru>
2021-01-26 11:00:43 +0300

    With npm

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/site', 'SiteController@site')->name('site');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
